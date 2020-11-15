2631b725a39a71b42bbf84d82c8baf4f6651132b
Matthew Bundy <mattb@protechmn.com>
2020-11-15 13:14:26 -0600

    Added new functionality. Fixed bug in dist_between_contours

--- calc.py
+++ calc.py
@@ -1,5 +1,6 @@
 import cv2
 import math
+import numpy as np
 
 def contour_center(c):
     """Returns the center point (pixels) of the given contour"""
@@ -13,8 +14,22 @@ x and y offset of the contours. (distance,dx,dy)"""
     center1 = contour_center(c1)
     center2 = contour_center(c2)
     dx = center1[0] - center2[0]
-    dy = center2[1] - center2[1]
+    dy = center1[1] - center2[1]
     d = math.sqrt(dx**2 + dy**2)
     return (d,dx,dy)
 
+def avg_dist(cnt_pairs):
+    """Takes a list of contour pairs and returns the average distance between
+each of them as well as the average dx and dy.
+ret: (avg dist, avg dx, avg dy)"""
+    mean_d = np.empty(len(cnt_pairs))
+    mean_dx = np.empty(len(cnt_pairs))
+    mean_dy = np.empty(len(cnt_pairs))
+    for i,pair in enumerate(cnt_pairs):
+        dist,dx,dy = dist_between_contours(pair[0],pair[1])
+        mean_d[i] = dist
+        mean_dx[i] = dx
+        mean_dy[i] = dy
+
+    return (mean_d.mean(), mean_dx.mean(), mean_dy.mean())
 
