0b465f2a53daede233b5ea6a6bbde45129932fad
Matthew Bundy <mattb@protechmn.com>
2020-11-15 05:51:42 -0600

    added recolor function

--- pre_process.py
+++ pre_process.py
@@ -7,3 +7,12 @@ def denoise(img, strength = 5):
     cv2.morphologyEx(img,cv2.MORPH_OPEN, kernel)
     return cv2.fastNlMeansDenoising(img, h=strength)
 
+
+def recolor(img,orig_color,new_color,threshold=10):
+    """Takes a grayscale image and changes orig_color to new_color if it is
+within a certain threshold. Returns a newly recolored image"""
+    ret = img.copy()
+    for x in np.nditer(ret, op_flags=['readwrite']):
+        if abs(x - orig_color) <= threshold:
+            x[...] = new_color
+    return ret
