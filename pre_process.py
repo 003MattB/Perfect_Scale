eb667a67b5ed7578d743f7fa86e150619cae6f71
Matthew Bundy <mattb@protechmn.com>
2020-11-15 13:15:23 -0600

    new functionality

--- pre_process.py
+++ pre_process.py
@@ -16,3 +16,18 @@ within a certain threshold. Returns a newly recolored image"""
         if abs(x - orig_color) <= threshold:
             x[...] = new_color
     return ret
+
+
+def parent_child_pairs(cnts,hierarchy):
+    """Takes a list of contours and a hierarchy tree and returns a list of
+parent child pairs of contours"""
+    
+    if len(hierarchy) != 1:
+        return []
+    ret = []
+    for n,p,child,parent in hierarchy[0]:
+        if (child | parent) != -1:
+            child_parent = hierarchy[0][child][3]
+            ret.append((cnts[child_parent],cnts[child]))
+
+    return ret
